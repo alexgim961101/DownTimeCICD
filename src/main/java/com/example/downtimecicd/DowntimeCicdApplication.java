808d34f4af6105f492720471771841afea5999fe
alexgim961101 <alexgim961101@gmail.com>
2023-10-19 01:13:49 +0900

    first commit

---
+++ src/main/java/com/example/downtimecicd/DowntimeCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.downtimecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowntimeCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DowntimeCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/downtimecicd/DowntimeCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.downtimecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DowntimeCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
