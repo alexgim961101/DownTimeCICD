17939a13a6034dc37bb1ed373ffce1e6db784351
alexgim961101 <alexgim961101@gmail.com>
2023-10-19 01:15:40 +0900

    배포준비

---
+++ src/main/java/com/example/downtimecicd/Controller.java
@@ -0,0 +1,16 @@
+package com.example.downtimecicd;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class Controller {
+
+    @GetMapping("/check")
+    public ResponseEntity<String> check() {
+        return ResponseEntity.ok("hello spring");
+    }
+}
